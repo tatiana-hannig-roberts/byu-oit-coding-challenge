3436d850e12303ac7d0bf6ae24c1adfaed072a85
Tatiana Roberts <tatianahannig@gmail.com>
2022-01-29 16:44:30 -0700

    uploading files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Byu-coding-challenge
+
+This application needs git and node.js installed in order to run.  Additionally the project needs packaged modules installed and the angular cli in order to run.  Steps are as follows:
+
+## Download and install git
+https://gitforwindows.org/
+
+## Download and install node.js
+https://nodejs.org/en/download/
+
+## At the project root open git bash
+
+## To install package modules run
+npm install
+
+## To install angular cli run
+npm install -g @angular/cli
+
+## To serve the project on localhost:4200 run
+ng serve
+
+
+The code used to complete this challenge is located in the src/app folder.
---
+++ src/app/app.component.ts
@@ -0,0 +1,136 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+
+import { shuffle } from 'lodash';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+	constructor(private http: HttpClient) { }
+	word = '';
+	letters = [];
+	chosenLetters = [];
+	submittedValidWords = [];
+	submittedInvalidWords = [];
+	buttonsDisabled = false;
+	boardDisabled = false;
+	difficultyOptions = [
+	  { name: 'Easy', minLength: 0, maxLength: 5 },
+	  { name: 'Medium', minLength: 6, maxLength: 7 },
+	  { name: 'Hard', minLength: 8, maxLength: 100 },
+    { name: 'Any', minLength: 0, maxLength: 100 }
+	];
+	chosenDifficulty;
+	startButtonText = 'Start Game';
+	activeGame = false;
+	wonGame = false;
+	quitGame = false;
+
+	resetBoard() {
+    this.chosenLetters = [];
+    this.letters.forEach((letter) => {
+      letter.used = false;
+      letter.active = false;
+      letter.hint = false;
+    });
+	}
+
+	getWords() {
+    this.chosenLetters = [];
+	  this.submittedValidWords = [];
+	  this.submittedInvalidWords = [];
+    const difficulty = this.difficultyOptions.find((option) => option.name === this.chosenDifficulty);
+		this.http.get('https://random-word-api.herokuapp.com/word?number=100').subscribe((response) => {
+		  this.wonGame = false;
+		  this.boardDisabled = false;
+		  this.word = response.find((word) => word.length >= difficulty.minLength && word.length <= difficulty.maxLength).toUpperCase();
+		  this.scrambleWord();
+		  this.buttonsDisabled = false;
+		  this.startButtonText = 'Restart Game';
+		  this.activeGame = true;
+		});
+	}
+
+	scrambleWord() {
+	  this.letters = shuffle(this.word.split('')).map((letter, i) => {
+      return { character: letter, index: i, used: false, active: false, hint: false };
+    });
+	}
+
+	selectLetter(selectedLetter) {
+	  if(!selectedLetter.used && !this.boardDisabled) {
+	    if(!selectedLetter.active) {
+	      this.letters.forEach((letter) => {
+	        letter.hint = false;
+	        if(letter.active) {
+	          letter.used = true;
+	        }
+	        letter.active = selectedLetter.index === letter.index;
+        });
+       this.chosenLetters.push(selectedLetter.index);
+	    } else {
+        this.chosenLetters.pop();
+        this.letters.forEach((letter) => {
+          letter.hint = false;
+          letter.active = letter.index === this.chosenLetters[this.chosenLetters.length - 1];
+          letter.used = letter.used && letter.active ? false : letter.used;
+        });
+	    }
+    }
+	}
+
+	isWordUsed() {
+	  return this.submittedValidWords.some((word) => word === this.convertToWord()) || this.submittedInvalidWords.some((word) => word === this.convertToWord());
+	}
+
+	sendHint() {
+	  if(!this.chosenLetters.length || this.word.indexOf(this.convertToWord().charAt(0)) !== 0) {
+      this.letters.find((letter) => this.word.indexOf(letter.character) === 0).hint = true;
+	  }
+    if(this.chosenLetters.length && this.word.indexOf(this.convertToWord()) === 0 && this.word !== this.convertToWord()) {
+      this.letters.find((letter) => letter.character === this.word.charAt(this.chosenLetters.length) && !letter.used && !letter.active).hint = true;
+    }
+	}
+
+	convertToWord() {
+	  return this.chosenLetters.map((index) => this.letters.find((letter) => letter.index === index).character).join('');
+	}
+
+	submitWord() {
+	  this.buttonsDisabled = true;
+	  if(this.convertToWord() === this.word) {
+	    this.submittedValidWords.push(this.convertToWord());
+	    this.boardDisabled = true;
+	    this.wonGame = true;
+	    this.startButtonText = 'Play Again';
+      this.letters.forEach((letter) => {
+        letter.used = true;
+        letter.active = false;
+        letter.hint = false;
+      });
+    } else {
+      this.http.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.convertToWord()}`).subscribe((response) => {
+        this.submittedValidWords.push(this.convertToWord());
+        this.resetBoard();
+        this.buttonsDisabled = false;
+
+      }, (error) => {
+        this.submittedInvalidWords.push(this.convertToWord());
+        this.resetBoard();
+        this.buttonsDisabled = false;
+      });
+    }
+	}
+
+	shutDown() {
+	  console.log('got here');
+	  this.activeGame = false;
+	  this.wonGame = false;
+	  this.quitGame = true;
+	}
+}
